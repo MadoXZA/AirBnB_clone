27a7acf137f5fe53c1a1fcce6740eec5ca9b9053
MadoXZA <timklo15@gmail.com>
2023-08-13 09:36:50 -0700

    Fixed File

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,11 +14,14 @@ class BaseModel_testing(unittest.TestCase):
     def testpep8(self):
         """ testing codestyle """
         pepstylecode = pep8.StyleGuide(quiet=True)
-        rest = pepstylecode.check_files(['models/base_model.py',
-                                         'models/__init__.py',
-                                         'models/engine/file_storage.py'])
-        self.assertEqual(rest.total_errors, 0,
-                         "Found code style errors (and warnings).")
+        checked_files = [
+            'models/base_model.py',
+            'models/__init__.py',
+            'models/engine/file_storage.py'
+        ]
+        rest = pepstylecode.check_files(checked_files)
+        error_msg = "Found code style errors (and warnings)."
+        self.assertEqual(rest.total_errors, 0, error_msg)
 
 
 class test_for_base_model(unittest.TestCase):
@@ -42,9 +45,8 @@ class test_for_base_model(unittest.TestCase):
 
     def test_kwargs_constructor_2(self):
         """ check id with data """
-        dictonary = {'score': 100}
-
-        object_test = BaseModel(**dictonary)
+        dictionary = {'score': 100}
+        object_test = BaseModel(**dictionary)
         self.assertTrue(hasattr(object_test, 'id'))
         self.assertTrue(hasattr(object_test, 'created_at'))
         self.assertTrue(hasattr(object_test, 'updated_at'))
@@ -52,32 +54,34 @@ class test_for_base_model(unittest.TestCase):
 
     def test_str(self):
         """ Test string """
-        dictonary = {'id': 'cc9909cf-a909-9b90-9999-999fd99ca9a9',
-                     'created_at': '2025-06-28T14:00:00.000001',
-                     '__class__': 'BaseModel',
-                     'updated_at': '2030-06-28T14:00:00.000001',
-                     'score': 100
-                     }
-
-        object_test = BaseModel(**dictonary)
-        out = "[{}] ({}) {}\n".format(type(object_test).__name__, object_test.id, object_test.__dict__)
+        dictionary = {
+            'id': 'cc9909cf-a909-9b90-9999-999fd99ca9a9',
+            'created_at': '2025-06-28T14:00:00.000001',
+            '__class__': 'BaseModel',
+            'updated_at': '2030-06-28T14:00:00.000001',
+            'score': 100
+        }
+        object_test = BaseModel(**dictionary)
+        expected_output = "[{}] ({}) {}\n".format(
+            type(object_test).__name__,
+            object_test.id,
+            object_test.__dict__
+        )
 
     def test_to_dict(self):
         """ check dict """
         object_test = BaseModel(score=300)
         n_dict = object_test.to_dict()
-
         self.assertEqual(n_dict['id'], object_test.id)
         self.assertEqual(n_dict['score'], 300)
         self.assertEqual(n_dict['__class__'], 'BaseModel')
         self.assertEqual(n_dict['created_at'], object_test.created_at.isoformat())
         self.assertEqual(n_dict['updated_at'], object_test.updated_at.isoformat())
-
         self.assertEqual(type(n_dict['created_at']), str)
         self.assertEqual(type(n_dict['created_at']), str)
 
     def test_datetime(self):
-        """ check datatime """
+        """ check datetime """
         bas1 = BaseModel()
         self.assertFalse(datetime.now() == bas1.created_at)
 
@@ -87,19 +91,10 @@ class test_for_base_model(unittest.TestCase):
         self.my_model.my_number = 100
         self.my_model.save()
         my_model_json = self.my_model.to_dict()
-
-        self.assertEqual(
-            self.my_model.name, my_model_json['name']
-        )
-        self.assertEqual(
-            self.my_model.my_number, my_model_json['my_number']
-        )
-        self.assertEqual(
-            'BaseModel', my_model_json['__class__']
-        )
-        self.assertEqual(
-            self.my_model.id, my_model_json['id']
-        )
+        self.assertEqual(self.my_model.name, my_model_json['name'])
+        self.assertEqual(self.my_model.my_number, my_model_json['my_number'])
+        self.assertEqual('BaseModel', my_model_json['__class__'])
+        self.assertEqual(self.my_model.id, my_model_json['id'])
 
     def test_savefirst(self):
         """check numbers"""
@@ -114,7 +109,7 @@ class test_for_base_model(unittest.TestCase):
     def test_inst(self):
         """check class """
         ml = BaseModel()
-        self.assertIsInstance(ml, BaseModel)
+        self.assertTrue(ml, BaseModel)
 
 
 if __name__ == '__main__':
