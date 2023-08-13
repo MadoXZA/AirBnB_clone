15a204a719bcfd770b272addc9258941963bed13
EsliNsengiyumva <eslingiyumva@gmail.com>
2023-08-12 21:19:01 -0700

    test unit

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-""" testing Amenity """
+"""Testing Amenity"""
+
 import unittest
 import pep8
 from models.amenity import Amenity
-class Amenity_testing(unittest.TestCase):
- """ check BaseModel """
- def testpep8(self):
- """ testing codestyle """
- pepstylecode = pep8.StyleGuide(quiet=True)
- path_user = 'models/amenity.py'
- result = pepstylecode.check_files([path_user])
- self.assertEqual(result.total_errors, 0,
- "Found code style errors (and warnings).")
+
+class AmenityTesting(unittest.TestCase):
+    """Check Amenity class"""
+
+    def test_pep8(self):
+        """Test code style"""
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_amenity = 'models/amenity.py'
+        result = pepstylecode.check_files([path_amenity])
+        self.assertEqual(result.total_errors, 0, "Found code style errors (and warnings).")
+
+if __name__ == '__main__':
+    unittest.main()
+
