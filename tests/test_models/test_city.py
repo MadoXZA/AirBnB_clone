2380e5ca1c393df309da1ee449841295c5210e0e
EsliNsengiyumva <eslingiyumva@gmail.com>
2023-08-12 22:01:20 -0700

    test updates

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-""" testing city """
+"""Testing city"""
+
 import unittest
 import pep8
 from models.city import City
-class City_testing(unittest.TestCase):
- """ check BaseModel """
- def testpep8(self):
- """ testing codestyle """
- pepstylecode = pep8.StyleGuide(quiet=True)
- path_user = 'models/city.py'
- result = pepstylecode.check_files([path_user])
- self.assertEqual(result.total_errors, 0,
- "Found code style errors (and warnings).")
+
+class CityTesting(unittest.TestCase):
+    """Check City class"""
+
+    def test_pep8(self):
+        """Test code style"""
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_city = 'models/city.py'
+        result = pepstylecode.check_files([path_city])
+        self.assertEqual(result.total_errors, 0, "Found code style errors (and warnings).")
+
+if __name__ == '__main__':
+    unittest.main()
+
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-""" testing User """
+"""Testing User"""
+
 import unittest
 import pep8
 from models.user import User
-class User_testing(unittest.TestCase):
- """ check BaseModel """
- def testpep8(self):
- """ testing codestyle """
- pepstylecode = pep8.StyleGuide(quiet=True)
- path_user = 'models/user.py'
- result = pepstylecode.check_files([path_user])
- self.assertEqual(result.total_errors, 0,
- "Found code style errors (and warnings).")
+
+class UserTesting(unittest.TestCase):
+    """Check User class"""
+
+    def test_pep8(self):
+        """Test code style"""
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_user = 'models/user.py'
+        result = pepstylecode.check_files([path_user])
+        self.assertEqual(result.total_errors, 0, "Found code style errors (and warnings).")
+
+if __name__ == '__main__':
+    unittest.main()
+
